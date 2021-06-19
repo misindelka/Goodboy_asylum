bdc0513acce31e963482c66d18816a43328df94f
misindelka <misindelka.dev@gmail.com>
2021-06-19 18:13:20 +0200

    setup commit
    react, redux, react-redux, redux- thunk, redux-dev-tools, eslint, typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as theme from './styles/theme';
+import store from './redux/store';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <ThemeProvider theme={theme}>
+            <Provider store={store}>
+                <Suspense fallback="...Loading">
+                    <App />
+                </Suspense>
+            </Provider>
+        </ThemeProvider>
     </React.StrictMode>,
     document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
